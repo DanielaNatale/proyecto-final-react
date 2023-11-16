e5a0a9c9a3bafb1ada561857d613713f9729c5e5
Daniela Natale <daniela.natale13@gmail.com>
2023-11-16 11:56:11 +0100

    clase de practica react

---
+++ src/componentes/footer.js
@@ -0,0 +1,7 @@
+const Footer = props =>{
+    return(
+        <h2>Este es el pie de la pagina</h2>
+    )
+}
+
+export default Footer;
---
+++ src/componentes/header.js
@@ -0,0 +1,7 @@
+const Header = props =>{
+    return(
+        <h2>Esta es la cabecera</h2>
+    )
+}
+
+export default Header;
